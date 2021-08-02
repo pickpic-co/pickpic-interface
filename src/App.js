f11233ab2cf72cd2ce5a289aa3b7464e78cdd1dd
aeropriest <ashok@myhomi.io>
2021-08-02 19:08:55 +0800

    Added the buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import {ReactComponent as ShareButton} from './assets/share-button.svg';
 
 function App() {
   return (
       <div className="celebrity-hero-background">
-        <div class="celebrity-hero-name">Maria Sharapova</div>
-        <div class="celebrity-handle">@Maria Sharapova</div>
-        <div class="celebrity-followers">8.8m followers</div>
+        <div class="heading-text" style={{marginLeft: '2vw', marginTop: '25vh'}}>Maria Sharapova</div>
+        <div class="small-text" style={{marginLeft: '2vw'}}>@MariaSharapova</div>
+        <div class="small-text" style={{marginLeft: '2vw', marginTop: '1vh'}}>8.8m followers</div>
+        <button class="button share-button" style={{marginLeft: '2vw', marginTop: '1.5vh'}}></button>
+        <div class="small-text" style={{marginLeft: '80vw', marginTop: '18vh'}}>Highest Bid</div>
+        <div class="heading-text" style={{marginLeft: '80vw'}}>8.00ETH</div>
+        <div class="small-text" style={{marginLeft: '80vw'}}>8h 8m left</div>
+        <button class="button bid-button" style={{marginLeft: '80vw', marginTop: '1.5vh'}}>BID NOW !&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
       </div>
     );
 }
