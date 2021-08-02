0cf298b2cc6f8779f80b990b43a9811e9873ddd5
aeropriest <ashok@myhomi.io>
2021-08-02 20:33:06 +0800

    Added bottom scroll list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
+  return (    
       <div className="celebrity-hero-background">
         <div class="heading-text" style={{marginLeft: '2vw', marginTop: '25vh'}}>Maria Sharapova</div>
         <div class="small-text" style={{marginLeft: '2vw'}}>@MariaSharapova</div>
@@ -9,8 +9,26 @@ function App() {
         <button class="button share-button" style={{marginLeft: '2vw', marginTop: '1.5vh'}}></button>
         <div class="small-text" style={{marginLeft: '80vw', marginTop: '18vh'}}>Highest Bid</div>
         <div class="heading-text" style={{marginLeft: '80vw'}}>8.00ETH</div>
-        <div class="small-text" style={{marginLeft: '80vw'}}>8h 8m left</div>
+        <div class="small-text" style={{marginLeft: '80vw', textTransform: 'lowercase'}}><b>8</b> h <b>8</b> m left</div>
         <button class="button bid-button" style={{marginLeft: '80vw', marginTop: '1.5vh'}}>BID NOW !&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+        <div class="wrapper" style={{marginTop: '1vh'}}>
+          <div class="item">one</div>
+          <div class="item">two</div>
+          <div class="item">three</div>
+          <div class="item">four</div>
+          <div class="item">one</div>
+          <div class="item">two</div>
+          <div class="item">three</div>
+          <div class="item">four</div>
+          <div class="item">one</div>
+          <div class="item">two</div>
+          <div class="item">three</div>
+          <div class="item">four</div>
+          <div class="item">one</div>
+          <div class="item">two</div>
+          <div class="item">three</div>
+          <div class="item">four</div>
+        </div>
       </div>
     );
 }
