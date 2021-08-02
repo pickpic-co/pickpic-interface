24339182f924df563db2a3f0e735ab530d31b418
Ashok Jaiswal <ashok@lidqid.com>
2021-08-02 15:26:33 +0800

    Boiler plate code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {ReactComponent as ShareButton} from './assets/share-button.svg';
+
+function App() {
+  return (
+      <div className="celebrity-hero-background">
+        <div class="celebrity-hero-name">Maria Sharapova</div>
+        <div class="celebrity-handle">@Maria Sharapova</div>
+        <div class="celebrity-followers">8.8m followers</div>
+      </div>
+    );
+}
+
+export default App;
