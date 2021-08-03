7970648116d8f3c452646cc899eb1f09d431d508
sachin <skodagoda@apm.mc>
2021-08-03 20:49:10 +0800

    ✅ toggle action

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+export const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+
+export const isLoggedIn = () => {
+    return {
+        type: 'IS_LOGGIN'
+    }
+}
+
+
+
+export const showProductSearch = () => {
+    return {
+        type: 'SHOW_PRODUCT_SEARCH_COMPONENT'
+    }
+}
