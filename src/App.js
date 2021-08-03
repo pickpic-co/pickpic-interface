bec9f87904cb08724493d4bc329434b765c4d5ce
Ashok Jaiswal <ashok@lidqid.com>
2021-08-03 18:40:02 +0800

    Added horizontal menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import './App.css';
+import React from 'react';
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+
+
+const getItems = () =>
+  Array(20)
+    .fill(0)
+    .map((_, ind) => ({ id: `element-${ind}` }));
+
 
 function App() {
+  const [items, setItems] = React.useState(getItems);
+  const [selected, setSelected] = React.useState([]);
+  const [position, setPosition] = React.useState(0);
+
+  const isItemSelected = (id) => !!selected.find((el) => el === id);
+
+  const handleClick = (id) => ({ getItemById, scrollToItem }) => {
+    const itemSelected = isItemSelected(id)
+
+    setSelected((currentSelected) =>
+      itemSelected
+        ? currentSelected.filter((el) => el !== id)
+        : currentSelected.concat(id)
+    );
+  }
+
   return (    
       <div className="celebrity-hero-background">
         <div class="heading-text" style={{marginLeft: '2vw', marginTop: '25vh'}}>Maria Sharapova</div>
@@ -11,6 +36,21 @@ function App() {
         <div class="heading-text" style={{marginLeft: '80vw'}}>8.00ETH</div>
         <div class="small-text" style={{marginLeft: '80vw', textTransform: 'lowercase'}}><b>8</b> h <b>8</b> m left</div>
         <button class="button bid-button" style={{marginLeft: '80vw', marginTop: '1.5vh'}}>BID NOW !&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+        <ScrollMenu
+          //LeftArrow={LeftArrow}
+          //RightArrow={RightArrow}
+        >
+          {items.map(({ id }) => (
+            <Card
+              itemId={id} // NOTE: itemId is required for track items
+              title={id}
+              key={id}
+              onClick={handleClick(id)}
+              selected={isItemSelected(id)}
+            />)
+          )}
+
+        </ScrollMenu>        
         <div class="wrapper" style={{marginTop: '1vh'}}>
           <div class="item">one</div>
           <div class="item">two</div>
@@ -31,6 +71,56 @@ function App() {
         </div>
       </div>
     );
+}
+/*
+function LeftArrow() {
+  const { isFirstItemVisible, scrollPrev } = React.useContext(VisibilityContext)
+
+  return (
+    <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+      Left
+    </Arrow>
+  );
+}
+
+function RightArrow() {
+  const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext)
+
+  return (
+    <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
+      Right
+    </Arrow>
+  );
+}
+*/
+function Card({
+  onClick,
+  selected,
+  title,
+  itemId
+}) {
+  const visibility = React.useContext(VisibilityContext)
+
+  return (
+    <div
+      onClick={() => onClick(visibility)}
+      style={{
+        width: "160px",
+      }}
+      tabIndex={0}
+    >
+      <div className="card">
+        <div>{title}</div>
+        <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
+        <div>selected: {JSON.stringify(!!selected)}</div>
+      </div>
+      <div
+        style={{
+          height: "200px",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
