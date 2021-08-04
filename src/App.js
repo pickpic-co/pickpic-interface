029042b8f19df618ba0288d2c915d8fa8cc90dad
Ashok Jaiswal <ashok@lidqid.com>
2021-08-04 15:04:01 +0800

    Added the card row

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React from 'react';
+import './CelebrityCard.css'
+import CelebrityCard from './CelebrityCard.js'
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 
 
@@ -27,7 +29,7 @@ function App() {
   }
 
   return (    
-      <div className="celebrity-hero-background">
+      <div className="celebrity-hero-background">        
         <div class="heading-text" style={{marginLeft: '2vw', marginTop: '25vh'}}>Maria Sharapova</div>
         <div class="small-text" style={{marginLeft: '2vw'}}>@MariaSharapova</div>
         <div class="small-text" style={{marginLeft: '2vw', marginTop: '1vh'}}>8.8m followers</div>
@@ -36,63 +38,28 @@ function App() {
         <div class="heading-text" style={{marginLeft: '80vw'}}>8.00ETH</div>
         <div class="small-text" style={{marginLeft: '80vw', textTransform: 'lowercase'}}><b>8</b> h <b>8</b> m left</div>
         <button class="button bid-button" style={{marginLeft: '80vw', marginTop: '1.5vh'}}>BID NOW !&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
-        <ScrollMenu
+        <ScrollMenu 
           //LeftArrow={LeftArrow}
           //RightArrow={RightArrow}
         >
           {items.map(({ id }) => (
-            <Card
+            <CelebrityCard
+              img = 'https://wallpaperaccess.com/full/1484671.jpg'
               itemId={id} // NOTE: itemId is required for track items
-              title={id}
-              key={id}
+              name={id}
+              handle={id}
               onClick={handleClick(id)}
               selected={isItemSelected(id)}
             />)
           )}
 
         </ScrollMenu>        
-        <div class="wrapper" style={{marginTop: '1vh'}}>
-          <div class="item">one</div>
-          <div class="item">two</div>
-          <div class="item">three</div>
-          <div class="item">four</div>
-          <div class="item">one</div>
-          <div class="item">two</div>
-          <div class="item">three</div>
-          <div class="item">four</div>
-          <div class="item">one</div>
-          <div class="item">two</div>
-          <div class="item">three</div>
-          <div class="item">four</div>
-          <div class="item">one</div>
-          <div class="item">two</div>
-          <div class="item">three</div>
-          <div class="item">four</div>
-        </div>
       </div>
     );
 }
-/*
-function LeftArrow() {
-  const { isFirstItemVisible, scrollPrev } = React.useContext(VisibilityContext)
 
-  return (
-    <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
-      Left
-    </Arrow>
-  );
-}
 
-function RightArrow() {
-  const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext)
 
-  return (
-    <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
-      Right
-    </Arrow>
-  );
-}
-*/
 function Card({
   onClick,
   selected,
@@ -110,10 +77,12 @@ function Card({
       tabIndex={0}
     >
       <div className="card">
-        <div>{title}</div>
-        <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
-        <div>selected: {JSON.stringify(!!selected)}</div>
-      </div>
+        <div className="celebrity-card-background">
+          <div>{title}</div>
+          <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
+          <div>selected: {JSON.stringify(!!selected)}</div>
+        </div>
+        </div>
       <div
         style={{
           height: "200px",
--- src/CelebrityCard.js
+++ src/CelebrityCard.js
@@ -1,1 +1,22 @@
 //https://www.youtube.com/watch?v=4KxHcbQ8GYQ
+import React from 'react';
+
+function CelebrityCard(props){
+    return(
+      <div className="CelebrityCard">
+        <div className="CardBody">
+          <div class="small-text" style={{marginLeft: '2vw'}}>{props.name}</div>
+          <div class="small-text" style={{marginLeft: '2vw'}}>{props.handle}</div>
+          <h2 className="celebrityName">{props.name}</h2>
+          <h3 className="celebrityName">{props.handle}</h3>
+          <p className="celebrityFollowers">{props.followers}</p>
+        </div>
+        <button className="celebrityCardButton">
+          BID NOW
+          </button>
+      </div>
+    )
+  }
+//          <img src={props.img} class="celebrityCardImage"/>
+
+  export default CelebrityCard;
