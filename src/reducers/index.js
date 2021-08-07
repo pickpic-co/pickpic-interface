01a5371059d7335e4567f5604519ffe157393b43
sachin <skodagoda@apm.mc>
2021-08-07 08:31:58 +0800

    🚧 organized reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,15 @@
 import counterReducer from "./counter";
 import loggedReducer from "./isLogged";
 import { showProductSearchReducer}  from "./showProductSearch";
+import products from "./products";
+
 import { combineReducers } from "redux";
 
 const allReducers = combineReducers({
     counter:counterReducer,
     isLogged:loggedReducer,
-    showProductSearch:showProductSearchReducer
+    showProductSearch:showProductSearchReducer,
+    products
 })
 
 export default allReducers
---
+++ src/reducers/products.js
@@ -0,0 +1,44 @@
+import {
+    CREATE_PRODUCT,
+    RETRIEVE_PRODUCTS,
+    UPDATE_PRODUCT,
+    DELETE_PRODUCT,
+    DELETE_ALL_PRODUCTS
+  } from "..actions/types";
+
+  const initialState = [];
+
+function ProductReducer(PRODUCTS = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CREATE_PRODUCT:
+      return [...PRODUCTS, payload];
+
+    case RETRIEVE_PRODUCTS:
+      return payload;
+
+    case UPDATE_PRODUCT:
+      return PRODUCTS.map((PRODUCT) => {
+        if (PRODUCT.id === payload.id) {
+          return {
+            ...PRODUCT,
+            ...payload,
+          };
+        } else {
+          return PRODUCT;
+        }
+      });
+
+    case DELETE_PRODUCT:
+      return PRODUCTS.filter(({ id }) => id !== payload.id);
+
+    case DELETE_ALL_PRODUCTS:
+      return [];
+
+    default:
+      return PRODUCTS;
+  }
+};
+
+export default ProductReducer;
